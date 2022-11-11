a8512823de26afce7f2d1d4097fdc86effbcb7f6
DeNatur <szymon.stasik1@gmail.com>
2022-11-11 12:50:23 +0100

    Add machine info to view

--- Assets/Scripts/views/MachineView.cs
+++ Assets/Scripts/views/MachineView.cs
@@ -2,6 +2,7 @@ using Microsoft.MixedReality.Toolkit.UI;
 using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
 using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 public class MachineView : MonoBehaviour
@@ -9,6 +10,12 @@ public class MachineView : MonoBehaviour
     public Interactable addAnchorButton;
     public Interactable removeAnchorButton;
     public Interactable tapToPlaceButton;
+    public Text nameText;
+    public Text symbolText;
+    public Text statusText;
+    public Text hallIdText;
+    public Text efficiencyText;
+    public Text technicalExaminationDateText;
     public Interactable deleteButton;
     private TapToPlace tapToPlaceScript;
     private MachinePresenter _anchorPresenter;
@@ -35,6 +42,34 @@ public class MachineView : MonoBehaviour
         removeAnchorButton.gameObject.SetActive(state.isRemoveAnchorVisible);
         tapToPlaceButton.gameObject.SetActive(state.isTapToPlaceVisible);
         deleteButton.gameObject.SetActive(state.isDeleteMachineVisible);
+
+        if (state.machineInfo != null)
+        {
+            nameText.text = string.Format(
+                "Machine Name: %s",
+                state.machineInfo.Value.name
+                );
+            symbolText.text = string.Format(
+                "Symbol: %s",
+                state.machineInfo.Value.symbol
+                );
+            statusText.text = string.Format(
+                "Status: %s",
+                state.machineInfo.Value.status
+                );
+            hallIdText.text = string.Format(
+                "Status: %d",
+                state.machineInfo.Value.hallId
+                );
+            efficiencyText.text = string.Format(
+                "Efficiency: %d",
+                state.machineInfo.Value.efficiency
+                );
+            technicalExaminationDateText.text = string.Format(
+                "Technical Examination Date: %s",
+                state.machineInfo.Value.technicalExaminationDate.ToString("MM/dd/yyyy HH:mm:ss")
+                );
+        }
     }
 
     private void AnchorPresenter_DeleteObject()
